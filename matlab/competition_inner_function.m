7d708b1811ab1ac1f14efeb8ca843afeb77d53dd
Konstantinos Pitas <konstantinos.pitas@epfl.ch>
2016-12-20 20:53:03 +0100

    Competition Script

---
+++ matlab/competition_inner_function.m
@@ -0,0 +1,57 @@
+function [x, h, b] = competition_inner_function(Y, chain_c,iter_c,calc_ham)
+% SIMMULATED_ANNEALING recovers the vector x generating the non-linear 
+% noisy observation Y by running a chain_type Markov chain from initial
+% state x0.
+% 
+%   Usage:
+%       [x, h, b] = competition_function(Y, chain_type,time)
+%
+%   Input:
+%       Y : matrix
+%           (Optional) Initial state of the annealing.
+%       chain_type : Glauber 
+%                    or Metropolis
+%       iter_c : number of max iterations 
+%              for execution
+%       calc_ham : calculate hamiltonian or not
+%   Output:
+%       x : vector
+%           Recovered vector.
+%       h : vector
+%           The evaluation of the Hamiltonian at the fihnal iteration.
+%
+%
+%   References:
+%       
+%
+% Author(s): Pitas Konstantinos
+% Date : 20/12/2016
+% Testing: test_competition_function.m
+
+%% Setup
+N = size(Y,2);
+lambda = 0.01 * N.^2;
+beta0 = 1e-3;
+%beta0 = 0.12/(1.05^iter_c);
+chain_type = chain_c; %'metropolis' 'glauber'
+param = struct('maxit_anneal', 100, ...
+               'maxit', iter_c, ...
+               'tol_anneal', 0, ...
+               'tol', 0);
+
+%% Define the hamiltonian and beta_update functions
+ham = @(x) hamiltonian(x, Y, lambda); 
+bup = @(b, n) inv_temp_fun(b, n, 'exp');
+
+%% Run annealing
+[x, ~, b] = simulated_annealing([], Y, lambda, beta0, ham, bup, ...
+                                 chain_type, param);
+
+%%Calculate Hamiltonian
+if calc_ham ==1
+    h=ham(x);
+else
+    h=1;
+end
+
+end
